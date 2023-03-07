78a8b5f3286e5e43991c6b64b67c38b1c3623c3e
kuba <Kudaiberdigapyrov@gmail.com>
2023-03-07 16:14:17 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Scanner scanner = new Scanner(System.in);
+        int myint= scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = myint*b;
+        System.out.println(sum);*/
+
+        Scanner scanner = new Scanner(System.in);
+        int sum = 5 + 7 + 8;
+        System.out.println(sum);
+        int sum1 = 5 * 7 * 8;
+        System.out.println(sum1);
+        double sum2 = (5 + 7 / 8  ) % 3;
+        System.out.println(sum2);
+    }
+}
